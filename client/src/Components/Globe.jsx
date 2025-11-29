6829c58f6012429ec11e03e6b8b1f124417f9b10
Amogh-007-Rin <amoghdath233@gmail.com>
2025-11-29 14:06:03 +0000

    Updated-Fronend

---
+++ client/src/Components/Globe.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function TopElement() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/JYSToDyGQlIo9aP1/scene.splinecode" 
+      />
+    </main>
+  );
+}
